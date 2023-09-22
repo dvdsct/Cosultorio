c1bebb412e17423e98d81a79c2427d3114e247cc
Vale <valen_m95@hotmail.com>
2023-09-22 20:26:52 -0300

    [ADD]Seeders

---
+++ database/seeders/OsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
---
+++ database/seeders/PacienteSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Persona;
+use App\Models\Perfil;
+
+class PacienteSeeder extends Seeder
+{
+
+
+
+
+    public function run(): void
+    {
+          $personas =[
+            'David',
+            'Nahuel',
+            'Saso',
+            'Valentina'
+          ];
+        foreach ($personas as $persona){
+
+            Persona::create([
+
+                'nombre'=> $persona,
+                'apellido'=> 'Perez',
+                'fecha_de_nacimiento'=> '1995-11-11',
+                'dni'=> '3984849',
+                'ocupacion' => 'estudiante',
+                'tipo'=> 'fisica',
+                'estado'=> '1'
+            ]);
+        }
+
+              Perfil::create([
+
+                  'persona_id'=> '1',
+                  'descripcion'=> 'paciente',
+                  'estado'=> '1'
+              ]);
+
+              Perfil::create([
+
+                  'persona_id'=> '2',
+                  'descripcion'=> 'paciente',
+                  'estado'=> '1'
+              ]);
+
+              Perfil::create([
+
+                  'persona_id'=> '3',
+                  'descripcion'=> 'paciente',
+                  'estado'=> '1'
+              ]);
+
+              Perfil::create([
+
+                  'persona_id'=> '3',
+                  'descripcion'=> 'paciente',
+                  'estado'=> '1'
+              ]);
+
+
+
+
+    }
+}
