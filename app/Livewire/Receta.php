8ee456ec4593ada567cd89d43416a50c10f54624
David <escatd@gmail.com>
2024-01-22 17:22:56 -0300

    EDIT Broadcasting-Receta

--- app/Livewire/Receta.php
+++ app/Livewire/Receta.php
@@ -4,16 +4,47 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Vademecum;
+use Livewire\Attributes\On;
 
 class Receta extends Component
 {
 
     public $vademecum;
-    public $remedio = array();
+    public $recetados = [];
+    public $modal = false;
+
+    #[On('modalOn')]
+    public function openModal()
+    {
+        // dd('aqui');
+
+        $this->modal = true;
+    }
+    public function closeModal()
+    {
+
+        $this->modal = false;
+    }
+
+    public function recetar($id)
+    {
+
+        $remedio = Vademecum::find($id);
+        $this->recetados[] = $remedio->toArray();
+    }
+    public function receta()
+    {
+
+        dd($this->recetados);
+    }
+
+
 
     public function render()
     {
         $this->vademecum = Vademecum::all();
-        return view('livewire.receta');
+        return view('livewire.receta', [
+            'recetados' => $this->recetados
+        ]);
     }
 }
