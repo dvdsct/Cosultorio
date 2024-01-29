01c2ce2f683359cf12e6dafcc2bade4dde9cb730
NahuelMartinez16 <nahuelmartinez07@gmail.com>
2024-01-29 17:31:23 -0300

    [EDIT] Citologia-Biosia-Hallazgos

--- app/Livewire/FormColp.php
+++ app/Livewire/FormColp.php
@@ -21,35 +21,41 @@ class FormColp extends Component
     public $localidad;
 
     public $vph;
-    public $ascus;
-    public $lsil;
-    public $hsil;
-    public $ca;
-    public $otros;
+
+    /* Citologia Variables  */
+    public $ascus='';
+    public $lsil='';
+    public $asch='';
+    public $hsil='';
+    public $ca='';
+    public $otros='';
 
     public $observaciones;
     public $evaluacion_adec;
     public $evaluacion_inadec;
     public $zona_trans;
 
-    public $hall_normales;
-    public $anormales1;
-    public $anormales2;
-    public $no_especifico;
-    public $sospecha_inv;
-    public $hall_varios;
-    public $evaluacion;
-    public $schiller;
+    /* Hallazgos variables */
+    public $hall_normales='';
+    public $anormales1='';
+    public $anormales2='';
+    public $no_especifico='';
+    public $sospecha_inv='';
+    public $hall_varios='';
+    public $biopsia='';
+    public $evaluacion='';
+    public $schiller='';
 
-    public $negat;
-    public $cin1;
-    public $cin2;
-    public $cin3;
-    public $cis;
-    public $ca_inv;
-    public $adenocis;
-    public $adeno_ca_inv;
-    public $bio_otros;
+    /* Biopsia variables */
+    public $negat='';
+    public $cin1='';
+    public $cin2='';
+    public $cin3='';
+    public $cis='';
+    public $ca_inv='';
+    public $adenocis='';
+    public $adeno_ca_inv='';
+    public $bio_otros='';
     public $tratam;
     public $tratamientos;
     public $seguimiento;
@@ -67,39 +73,39 @@ class FormColp extends Component
     {
 
         $bio = new Biopsia;
-        $bio->negativo =  $this->negat ?? '1';
-        $bio->cin_1 = $this->cin1 ?? '1';
-        $bio->cin_2 = $this->cin2 ?? '1';
-        $bio->cin_3 = $this->cin3 ?? '1';
-        $bio->cis = $this->cis ?? '1';
-        $bio->ca_invasor = $this->ca_inv ?? '1';
-        $bio->adenocis = $this->adenocis ?? '1';
-        $bio->ac_invasor = $this->adeno_ca_inv ?? '1';
-        $bio->otros = $this->bio_otros ?? '1';
+        $bio->negativo =  $this->negat;
+        $bio->cin_1 = $this->cin1;
+        $bio->cin_2 = $this->cin2;
+        $bio->cin_3 = $this->cin3;
+        $bio->cis = $this->cis;
+        $bio->ca_invasor = $this->ca_inv;
+        $bio->adenocis = $this->adenocis;
+        $bio->ac_invasor = $this->adeno_ca_inv;
+        $bio->otros = $this->bio_otros;
         $bio->estado = '1';
         $bio->save();
 
         $cito = new Citologia;
-        $cito->asc_us = $this->ascus ?? '1';
-        $cito->l_sil = $this->lsil ?? '1';
-        $cito->asc_h = $this->asch ?? '1';
-        $cito->hsil = $this->hsil ?? '1';
-        $cito->ca = $this->ca ?? '1';
-        $cito->otros = $this->otros ?? '1';
+        $cito->asc_us = $this->ascus;
+        $cito->l_sil = $this->lsil;
+        $cito->asc_h = $this->asch;
+        $cito->hsil = $this->hsil;
+        $cito->ca = $this->ca;
+        $cito->otros = $this->otros;
         $cito->estado = '1';
         $cito->save();
 
 
         $hallaz = new Hallazgo;
-        $hallaz->normales = $this->hall_normales ?? '1';
-        $hallaz->anormales_g1 = $this->anormales1 ?? '1';
-        $hallaz->anormales_g2 = $this->anormales2 ?? '1';
-        $hallaz->no_especifico = $this->no_especifico ?? '1';
-        $hallaz->sospecha_inv = $this->sospecha_inv ?? '1';
-        $hallaz->varios = $this->hall_varios ?? '1';
-        $hallaz->biopsia = $this->biopsia ?? '1';
-        $hallaz->ecc = $this->evaluacion ?? '1';
-        $hallaz->test_schiller = $this->schiller ?? '1';
+        $hallaz->normales = $this->hall_normales;
+        $hallaz->anormales_g1 = $this->anormales1;
+        $hallaz->anormales_g2 = $this->anormales2;
+        $hallaz->no_especifico = $this->no_especifico;
+        $hallaz->sospecha_inv = $this->sospecha_inv;
+        $hallaz->varios = $this->hall_varios;
+        $hallaz->biopsia = $this->biopsia;
+        $hallaz->ecc = $this->evaluacion;
+        $hallaz->test_schiller = $this->schiller;
         $hallaz->estado = '1';
         $hallaz->save();
 
