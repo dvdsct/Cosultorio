75f8f90adf3f8bd79f9a030cbd2eb4c501e3f641
NahuelMartinez16 <nahuelmartinez07@gmail.com>
2024-02-28 13:46:53 -0300

    [EDIT] Tablas_cons_Paps_colpos

--- resources/views/Consultorio/Pacientes/index.blade.php
+++ resources/views/Consultorio/Pacientes/index.blade.php
@@ -8,16 +8,21 @@
 
 @section('content')
 
-<div class="card-header col-md-6 pt-4 border-0 px-1" style="display:flex;">
-    <div class="card-tools" style="width: 80%;">
-        <div class="input-group input-group-sm">
-            <input type="text" wire:model.live="query" class="form-control float-right" placeholder="Buscar paciente">
-            <div class="input-group-append">
-                <button type="submit" class="btn btn-default">
-                    <i class="fas fa-search"></i>
-                </button>
+<div class="row px-3">
+    <div class="card-header col-md-6 pt-4 border-0 px-1" style="display:flex;">
+        <div class="card-tools" style="width: 80%;">
+            <div class="input-group input-group-sm">
+                <input type="text" wire:model.live="query" class="form-control float-right" placeholder="Buscar paciente">
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
             </div>
         </div>
+    </div>
+    <div class="col-md-6" style="display: flex; justify-content:flex-end; align-items:flex-end">
+        <h2 style="margin: 0;"> <strong> PACIENTES </strong></h2>
     </div>
 </div>
 
