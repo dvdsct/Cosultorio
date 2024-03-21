915fa58b31130cffd396613d2c44efb6b8e7a111
NahuelMartinez16 <nahuelmartinez07@gmail.com>
2024-03-21 17:32:34 -0300

    [EDIT] desestimar_commit

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900" style="min-height: calc(100vh + 100px)">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
     <div>
         {{ $logo }}
     </div>
